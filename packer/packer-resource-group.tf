7a4d495ffb2b248342ed5f04344b1cdbf582f553
Wagner Hitomi <wyhitomi@gmail.com>
2020-06-05 01:32:47 -0300

    fix packer-resource-group
    Signed-off-by: Wagner Hitomi <wyhitomi@gmail.com>

--- packer/packer-resource-group.tf
+++ packer/packer-resource-group.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "vortx-images" {
   name     = "vortx-metabase-images"
   location = "Central US"
 }
